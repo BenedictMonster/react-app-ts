42fdcf5ede42ca48dcd42a9c97a68a055f26dc46
Benedict Monster <tech@benedictmonster.com>
2024-04-19 02:37:54 +0900

    Add an example to decrypt JWT token.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import base64 from "base-64";
+import logo from "./logo.svg";
+import "./App.css";
+
+function callApi() {
+  console.log("Page!!!!");
+  const response = fetch(
+    "http://alb-redate-2125372616.ap-northeast-2.elb.amazonaws.com/health-check/"
+  )
+    // the JSON body is taken from the response
+    .then((res) => res.json())
+    .then((res) => {
+      // The response has an `any` type, so we need to cast
+      // it to the `User` type, and return it from the promise
+      return res;
+    });
+  response.then((res) => console.log(res));
+}
+
+function getToken() {
+  console.log("getToken!!!!");
+  const payload = {
+    username: "admin",
+    password: "4321rewq",
+  };
+  const response = fetch(
+    "http://alb-redate-2125372616.ap-northeast-2.elb.amazonaws.com/auth/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    }
+  )
+    // the JSON body is taken from the response
+    .then((res) => res.json())
+    .then((res) => {
+      // The response has an `any` type, so we need to cast
+      // it to the `User` type, and return it from the promise
+      let accessPayload = res.refresh.substring(
+        res.refresh.indexOf(".") + 1,
+        res.refresh.lastIndexOf(".")
+      );
+      let accessDecrypted = base64.decode(accessPayload);
+      console.log(accessDecrypted);
+
+      let refreshPayload = res.refresh.substring(
+        res.refresh.indexOf(".") + 1,
+        res.refresh.lastIndexOf(".")
+      );
+      let refrechDecrypted = base64.decode(refreshPayload);
+      console.log(refrechDecrypted);
+      return res;
+    });
+  response.then((res) => console.log(res));
+}
 
 function App() {
   return (
@@ -18,6 +72,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={callApi}>Call API</button>
+        <button onClick={getToken}>getToken</button>
       </header>
     </div>
   );
